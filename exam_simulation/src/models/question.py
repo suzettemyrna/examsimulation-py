c462334478c7fd256e1e7d72464dbd55c788c8eb
suzettem <suzettem@student.21-school.ru>
2026-09-09 16:06:00 +0300

    rm old ver

--- exam_simulation/src/models/question.py
+++
@@ -1,5 +0,0 @@
-class Question:
-    def __init__(self, text):
-        self.text = text.strip()
-        self.words = text.split()
-        self.answered = 0
--- exam_simulation/src/models/student.py
+++
@@ -1,9 +0,0 @@
-from models.person import Person
-from models.enums import StudentStatus
-
-
-class Student(Person):
-    def __init__(self, name_and_gender):
-        self.name, self.gender = name_and_gender.split()
-        self.status = StudentStatus.QUEUE
-        self.time = 0
